c79c64ff22f1c1dce01b40947ce03b91f8b8ca24
Marco <msmo1960@gmail.com>
2021-06-06 12:51:50 -0500

    updated order

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.json());
-
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
